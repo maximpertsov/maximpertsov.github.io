bd8f628e214bf10ed805275d62cb015cf8828474
Maxim Pertsov <maxim.pertsov@gmail.com>
2015-07-13 09:37:41 -0400

    Fixed some typos in Asteroid description

--- js/views/container.js
+++ js/views/container.js
@@ -17,8 +17,8 @@ var Container = React.createClass({
               <Project name="Asteroid"
                        gif="assets/asteroid.gif"
                        href="https://github.com/maximpertsov/asteroid"
-                       text="Basic asteroid game, with slightly more explosive weaponry, and
-                          yarnballs instead on asteroids. Inspired by the RiceRocks project from
+                       text="Asteroid clone with slightly more explosive weaponry and
+                          yarnballs instead of asteroids. Inspired by the RiceRocks project from
                           the Introduction to Interactive Programming in Python class on
                           Coursera. Implemented in Ruby using the Gosu 2D graphics library."/>
             </td>
